40451f70e1babd4d75afb4f833f9b811a47fdddf
ArclingML <arcling@Han-Tyumi.localdomain>
2021-03-08 14:54:23 -0800

    chart xaxis fixed

--- src/results.py
+++ src/results.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import statsmodels.api as sm
+import matplotlib.ticker as ticker
        
 def to_year(string):
   """
@@ -51,6 +52,7 @@ def plot(df, p, X, y, name):
     """
     ax = df.plot(x='year', y=y, kind='scatter')
     ax.plot(X, p.const + p.year * X)
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
     ax.set_xlim([2000, 2022])
     ax.set_ylim([round(df[y].min()-0.1, 1), round(df[y].max()+0.1, 1)])
     fig = ax.get_figure()
